1554ca814fa78bc0e8fea7fd0aa50c4fdb9245c3
Garrett Thompson <drummergmt@gmail.com>
2022-01-10 15:50:56 -0500

    Submitted PewPewShooter

---
+++ README.md
@@ -0,0 +1,3 @@
+# PewPewShooter
+
+Developed with Unreal Engine 4
---
+++ Source/PewPewShooter/PewPewShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PewPewShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEWPEWSHOOTER_API APewPewShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
